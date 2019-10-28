b170ca958c9415afa3ef36fc684b088965be6f67
Felipe Passos <38237206+felipepassosc@users.noreply.github.com>
2019-10-28 09:28:08 -0300

    add ng-class e ng-click

--- oitavo-controller.js
+++ oitavo-controller.js
@@ -1,3 +1,7 @@
 angular.module('aplicacao').controller('OitavoController', function($scope){
     $scope.iniciado = true;
+
+    $scope.mudar = function(){
+        $scope.iniciado = false;
+    }
 })
