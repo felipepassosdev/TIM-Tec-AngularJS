e470f41743a47715b44911569c50daca841a441c
Felipe Passos <38237206+felipepassosc@users.noreply.github.com>
2019-11-06 16:08:01 -0300

    track by $index

--- decimo-controller.js
+++ decimo-controller.js
@@ -1,3 +1,7 @@
 angular.module('aplicacao').controller('DecimoController', function($scope){
-    
+    $scope.usuarios = []
+
+    $scope.submeterLogin = function() {
+        $scope.usuarios.push($scope.email)
+    }
 })
