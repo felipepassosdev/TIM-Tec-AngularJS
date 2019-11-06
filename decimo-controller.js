dad1787ed3c7cd8600ca43ea150c832b88c97e12
Felipe Passos <38237206+felipepassosc@users.noreply.github.com>
2019-11-06 16:58:33 -0300

    ng-minlength

--- decimo-controller.js
+++ decimo-controller.js
@@ -1,5 +1,6 @@
 angular.module('aplicacao').controller('DecimoController', function($scope){
     $scope.usuarios = []
+    $scope.iniciado = true
 
     $scope.submeterLogin = function() {
         $scope.usuarios.push($scope.email)
