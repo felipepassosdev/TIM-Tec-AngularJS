543a0045c7b9701e244b5768013f7600ac76a0d1
Felipe Passos <38237206+felipepassosc@users.noreply.github.com>
2019-11-07 14:35:13 -0300

    att

--- decimo-controller.js
+++ decimo-controller.js
@@ -1,12 +1,17 @@
 angular.module('aplicacao').controller('DecimoController', function($scope){
     $scope.usuarios = []
     $scope.iniciado = true
+    $scope.valido = false
+    $scope.hide = true
+    $scope.show = false
+    $scope.mensagem = 'As informações não estão corretas'
 
     $scope.submeterLogin = function() {
         if($scope.form_login.$valid){
             $scope.usuarios.push($scope.email)
         }else {
-
+            alert('NÃO VALIDO')
+            $scope.valido = true
         }
     }
 })
---
+++ decimoprimeiro-controller.js
@@ -0,0 +1,3 @@
+angular.module('aplicacao').controller('DecimoPrimeiroController', function($scope){
+
+})
